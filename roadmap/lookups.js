203b999563a8ad967b835906cae611fe08d794e8
Adi Eyal <adi@burgercom.co.za>
2014-12-29 22:28:43 +0200

    Adde JHB - Polokwane route to lookups.js

--- roadmap/lookups.js
+++ roadmap/lookups.js
@@ -38,10 +38,15 @@ var n1_cpt_jhb = [
 var n2_cpt_plet = [
     "CPT", "WC031", "WC034", "WC042", "WC043", "WC044", "WC048", "WC047",
     "EC109", "NMA", "EC106", "EC104", "EC126", "BUF", "EC123", "EC122", "EC121", "EC157", "EC155", "EC156", "EC442",
-    "KZN443", "KZN435", "KZN214", "KZN215", "KZN216", "KZN213", "KZN212", "ETH"
+    "KZN433", "KZN435", "KZN214", "KZN215", "KZN216", "KZN213", "KZN212", "ETH"
 ]
 
 var n3_jhb_eth = [
     "JHB", "EKU", "GT423", "GT422", "MP306", "FS205", "FS195", "FS194", "KZN235", "KZN232", "KZN236", "KZN234", "KZN223", "KZN222", "KZN225", "KZN226", "ETH"
 ]
+
+var n1_jhb_polokwane = [
+    "JHB", "TSH", "LIM366", "LIM365", "LIM364", "LIM367", "LIM354"
+]
+
 
