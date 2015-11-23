da2bfca23e8b02775e5bac19c1b855072c43812a
Adi Eyal <adi@code4sa.org>
2015-11-23 23:39:00 +0200

    Can now use a single file for embeds../route.html

--- times/libs/fines.js
+++ times/libs/fines.js
@@ -7,9 +7,10 @@ if (document.location.hostname == "localhost") {
 document.write('<script type="text/javascript" src="' + baseurl + 'assets/pym.js"></script>')
 
 elements = ["n1", "n2", "n3", "n2h"];
+document.write("<script>");
 for (idx in elements) {
     var el = elements[idx];
     if (document.getElementById(el))
-        document.write('<script>\
-            var pymParent = new pym.Parent("' + el + '", baseurl + "' + el + '.html", {});</script>')
+        document.write('var pymParent = new pym.Parent("' + el + '", ' + baseurl + '"route.html?route=' + el + '", {});')
 }
+document.write("</script>");
