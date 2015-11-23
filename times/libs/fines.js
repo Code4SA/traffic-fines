5a40d2cc16d8bda2f77a95cbcb2b8c7794f95ed4
Adi Eyal <adi@code4sa.org>
2015-11-23 23:57:08 +0200

    Enabled n1h

--- times/libs/fines.js
+++ times/libs/fines.js
@@ -6,7 +6,7 @@ if (document.location.hostname == "localhost") {
 
 document.write('<script type="text/javascript" src="' + baseurl + 'assets/pym.js"></script>')
 
-elements = ["n1", "n2", "n3", "n2h"];
+elements = ["n1", "n2", "n3", "n1h", "n2h"];
 document.write("<script>");
 for (idx in elements) {
     var el = elements[idx];
