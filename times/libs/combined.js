bc1c31fb950ef0020ba81bf1a6d2930b78b5b24f
Greg Kempe <greg@kempe.net>
2015-12-07 16:16:43 +0200

    Example of using Jekyll to build combined JS file
    This means we don't have a dependency on smash.
    However, since D3 uses's node's "import" statement,
    you'd need to use smash to get just the D3 files you want,
    then add that to combined.js

---
+++ times/libs/combined.js
@@ -0,0 +1,10 @@
+---
+---
+
+{% include_relative misc.js %}
+{% include_relative queue.v1.min.js %}
+{% include_relative template.js %}
+{% include_relative d3/d3.min.js %}
+{% include_relative d3-tooltip.js %}
+{% include_relative bubble_funcs.js %}
+{% include_relative main.js %}
